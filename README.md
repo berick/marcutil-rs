e01a24fac2c75361227c7edc07a6cef129d5b254
Bill Erickson <berickxx@gmail.com>
2022-09-08 11:00:34 -0400

    MARC for fun and profit
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ```rs
 // Parse an XML string
-let record = marc::Record::from_xml("<record/>").expect("Created record from XML");
+let record = marc::Record::from_xml(MARC_XML_STR).expect("Created record from XML");
 
 // Turn the record into Breaker text
 let breaker = record.to_breaker();
@@ -15,7 +15,6 @@ let record2 = marc::Record::from_breaker(&breaker).expect("Built from breaker");
 // Generate XML from our new record
 let xml = record2.to_xml().expect("To XML");
 
-// Make sure the new XML matches the source XML.
-assert_eq!("<record/>", xml);
+println!("Generated XML: {}", xml);
 ```
 
--- tests/marcutil.rs
+++ tests/marcutil.rs
@@ -10,6 +10,8 @@ use marcutil::Record;
 // Avoiding newlines / formatting for testing purposes.
 const MARC_XML: &str = r#"<?xml version="1.0"?><record xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"><leader>07649cim a2200913 i 4500</leader><controlfield tag="001">233</controlfield><controlfield tag="003">CONS</controlfield><controlfield tag="005">20140128084328.0</controlfield><controlfield tag="008">140128s2013    nyuopk|zqdefhi n  | ita d</controlfield><datafield tag="010" ind1=" " ind2=" "><subfield code="a">  2013565186</subfield></datafield><datafield tag="020" ind1=" " ind2=" "><subfield code="a">9781480328532</subfield></datafield><datafield tag="020" ind1=" " ind2=" "><subfield code="a">1480328537</subfield></datafield><datafield tag="024" ind1="1" ind2=" "><subfield code="a">884088883249</subfield></datafield><datafield tag="028" ind1="3" ind2="2"><subfield code="a">HL50498721</subfield><subfield code="b">Hal Leonard</subfield><subfield code="q">(bk.)</subfield></datafield></record>"#;
 
+const EMPTY_MARC_XML: &str = r#"<?xml version="1.0"?><record xmlns="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd"><leader></leader></record>"#;
+
 #[test]
 fn breaker_round_trip() {
 
@@ -45,8 +47,19 @@ fn all_round_trip() {
 
 #[test]
 fn odd_records() {
-    let op = Record::from_xml("<record/>");
+    let op = Record::from_xml(EMPTY_MARC_XML);
     assert!(op.is_ok());
+
+    let brk = op.unwrap().to_breaker();
+    assert_eq!(brk, "");
+
+    let op = Record::from_breaker(&brk);
+    assert!(op.is_ok());
+
+    let xml_op = op.unwrap().to_xml();
+    assert!(xml_op.is_ok());
+
+    assert_eq!(EMPTY_MARC_XML, xml_op.unwrap());
 
     let op = Record::from_xml(r#"<record><controlfield tag="123">"#);
     assert!(op.is_err());
